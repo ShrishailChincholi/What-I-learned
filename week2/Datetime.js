c0b6f7969d4550e49c3e0190661044b1cbea0867
Shrishail <shrishailchincholi306@gmail.com>
2025-07-12 23:09:39 +0530

    Date-Time project

---
+++ week2/Datetime.js
@@ -0,0 +1,22 @@
+let div = document.querySelector(".time");
+let date = document.querySelector(".date");
+
+
+//  adding Date,Time in webpage
+let update=() =>{
+    const now = new Date();
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    const s = String(now.getSeconds()).padStart(2, '0');
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+   div.innerText =`${h}:${m}:${s}`
+   date.innerText = ` ${day},  ${month + 1}, ${year}`;
+}
+
+// setInterval is  build for showing contuinue secondes
+setInterval(update,1000)
+
+// callinf fucation
+update();

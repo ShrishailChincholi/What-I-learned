a3e5202dc8dbd0adca90aadfdaa0ab4a5eeee652
Shrishail <shrishailchincholi306@gmail.com>
2025-07-13 15:29:29 +0530

    js-change

--- week2/JS-Projects/Date-Time/script.js
+++ week2/JS-Projects/Date-Time/script.js
@@ -5,18 +5,31 @@ let date = document.querySelector(".date");
 //  adding Date,Time in webpage
 let update=() =>{
     const now = new Date();
-    const h = String(now.getHours()).padStart(2, '0');
+
+    let arr=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
+    let daysname = arr[now.getDay()];
+    let h = now.getHours();
     const m = String(now.getMinutes()).padStart(2, '0');
     const s = String(now.getSeconds()).padStart(2, '0');
+
+    const ampm = h >= 12 ? 'PM' : 'AM';
+    h = h % 12 || 12;
     const day = now.getDate();
-    const month = now.getMonth();
+
+    const month =  ["January", "February", "March", "April", "May", "June", "July",
+                      "August", "September", "October", "November", "December"];
+    const monthName = month[now.getMonth()];
+
     const year = now.getFullYear();
-   div.innerText =`${h}:${m}:${s}`
-   date.innerText = ` ${day}, ${month + 1}, ${year}`;
+    div.innerText = `${h}:${m}:${s} ${ampm}`;  //${ampm}
+    date.innerText = `${daysname} ${day},  ${monthName}, ${year}`;
 }
 
 // setInterval is  build for showing contuinue secondes
 setInterval(update,1000)
 
-// callinf fucation
+// callinf funcation
 update();
+
+
+
--- week2/JS-Projects/mygame-RPS/script.js
+++ week2/JS-Projects/mygame-RPS/script.js
@@ -1,1 +1,58 @@
 alert("Explore the Game Dimension")
+
+// scroe starting value
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".game div");
+const msg = document.querySelector(".msg p");
+const userScoreEl = document.getElementById("user-score");
+const compScoreEl = document.getElementById("comp-score");
+
+// user choice to play the game
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.classList[0]; 
+    playGame(userChoice);
+  });
+});
+
+//  game logic
+function playGame(userChoice) {
+  // computer how get a random class 
+  const options = ['rock', 'paper', 'scissors'];
+  const compChoice = options[Math.floor(Math.random() * 3)];
+
+  if (userChoice === compChoice) {
+    showResult("Game is Draw!", userChoice, compChoice);
+     msg.style.backgroundColor="orange"
+  } else if (
+    (userChoice === 'rock' && compChoice === 'scissors') ||
+    (userChoice === 'paper' && compChoice === 'rock') ||
+    (userChoice === 'scissors' && compChoice === 'paper')
+  ) {
+    userScore++;
+    updateScore();
+    showResult("You Win", userChoice, compChoice);
+    msg.style.backgroundColor="green"
+  } else {
+    compScore++;
+    
+    updateScore();
+    showResult("You Lose! Paly again", userChoice, compChoice);
+     msg.style.backgroundColor="red"
+   
+  }
+}
+
+// updated scroe on user and computer
+function updateScore() {
+  userScoreEl.textContent = userScore;
+  compScoreEl.textContent = compScore;
+
+}
+
+function showResult(resultText, user, comp) {
+
+  msg.textContent = `${resultText} You: ${user}, Comp: ${comp}`;
+}

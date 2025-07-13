14ebd05179ca342a971f78ce377de8896a297ea7
Shrishail <shrishailchincholi306@gmail.com>
2025-07-13 20:44:39 +0530

    week3

---
+++ week3/Dom-JS/asssigment1.js
@@ -0,0 +1,35 @@
+// TO-DO TASK scripts
+
+let input = document.querySelector(".inp");
+let button = document.querySelector("#btn1");
+let list = document.querySelector(".added");
+
+// input = enter a list
+// button = add me
+// list = ul
+
+let addevents =()=>{
+    let stored=input.value;
+
+    let ulList= document.createElement("li");
+   
+    ulList.textContent=stored;
+
+    let btndelete = document.createElement("button");
+    btndelete.textContent ="delete"
+    list.appendChild(btndelete);
+    btndelete.addEventListener("click",()=>{
+        ulList.remove();
+      })
+
+    ulList.appendChild(btndelete);
+    list.appendChild(ulList)
+    
+    
+    input.value = "";
+
+   
+}
+// when we add a task click to button
+button.addEventListener("click",addevents)
+

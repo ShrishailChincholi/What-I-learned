13f6c67c33bd70fe927ee801458ce026270069eb
Shrishail <shrishailchincholi306@gmail.com>
2025-07-14 20:51:17 +0530

    asyncan&awati

--- week3/offline-video/async-await.js
+++ week3/offline-video/async-await.js
@@ -1,13 +1,22 @@
 // creating a promise
-function app(){
+function app(id){
     return promise = new Promise((reslove,reject)=>{
         setInterval(() => {
-            console.log("the data")
+            console.log("the data",id)
         }, 3000);  //1s=1000 ms
     });
 }
 
 // using aync-awati
 async function result() {
-    await app();
+    await app(1);
 }
+
+console.log(result);
+
+// we can call funcation immediately using IIFE-(Immediately Invoked Funcation)
+(async function result() {
+    await app(2);
+}) ();
+
+

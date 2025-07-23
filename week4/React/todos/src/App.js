a6b33956cbedec4be78ca283ec7dac5912d64f19
Shrishail <shrishailchincholi306@gmail.com>
2025-07-23 11:00:44 +0530

    React Todos completed

--- week4/React/todos/src/App.js
+++ week4/React/todos/src/App.js
@@ -1,19 +1,34 @@
-import Header from  './components/header';
-import TodoIteam from './components/toiteam';
+import React, { useState } from 'react';
+import Header from './components/Header.';
+import TodoItem from './components/TodoItem';
 import Button from './components/Button';
-import "./style.css";
+import './style.css';
 
 function App() {
-  return(
-    <div className='Todo-Container'>
-      <Header  />
-      <TodoIteam text="Eat"/>
-      <TodoIteam text="Go to colleage"/>
-      <TodoIteam text="Learning Props"/>
-      <Button />
+  const [todos, setTodos] = useState([
+    
+  ]);
+
+  const addTodo = (newTodo) => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="Todo-Container">
+      <Header />
+      {todos.map((todo, index) => (
+        <TodoItem key={index} text={todo} onDelete={() => deleteTodo(index)} />
+      ))}
+      <Button onAdd={addTodo} />
     </div>
-  )
- 
+  );
 }
 
 export default App;

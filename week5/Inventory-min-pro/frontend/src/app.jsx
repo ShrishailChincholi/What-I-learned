427df8bc226dedd6f3801bf272c391adaf951ff4
Shrishail <shrishailchincholi306@gmail.com>
2025-11-19 18:58:24 +0530

    code added

--- week5/Inventory-min-pro/frontend/src/app.jsx
+++ week5/Inventory-min-pro/frontend/src/app.jsx
@@ -3,16 +3,27 @@ import Login from "./components/login";
 import Register from "./components/register";
 import UserDashboard from "./components/user-dashboard";
 import AdminDashboard from "./components/admin-dashboard";
+import AddProduct from "./components/addproduct";
+import UpdateProduct from "./components/modifiyproduct";
+import ProductList from "./components/productlist";
 import "./style/login.regis.css"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} /> 
+
+        {/* Admin layout route */}
+        <Route path="/admin-dashboard" element={<AdminDashboard />}>
+          <Route path="add" element={<AddProduct />} />
+          <Route path="update/:id" element={<UpdateProduct />} />
+          <Route path="list" element={<ProductList />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
